98cf4cc76f41afdd648fa35cdfd9ee821ab83cc4
syzer3@gmail.com <syzer3@gmail.com>
2017-04-28 21:51:49 +0200

    re-format

--- models/Article.js
+++ models/Article.js
@@ -1,42 +1,42 @@
-var mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
-var slug = require('slug');
-var User = mongoose.model('User');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const slug = require('slug')
+const User = mongoose.model('User')
 
-var ArticleSchema = new mongoose.Schema({
+const ArticleSchema = new mongoose.Schema({
   slug: {type: String, lowercase: true, unique: true},
   title: String,
   description: String,
   body: String,
   favoritesCount: {type: Number, default: 0},
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  tagList: [{ type: String }],
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-}, {timestamps: true});
+  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+  tagList: [{type: String}],
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+}, {timestamps: true})
 
-ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
+ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'})
 
-ArticleSchema.pre('validate', function(next){
-  this.slugify();
+ArticleSchema.pre('validate', function (next) {
+  this.slugify()
 
-  next();
-});
+  next()
+})
 
-ArticleSchema.methods.slugify = function() {
-  this.slug = slug(this.title);
-};
+ArticleSchema.methods.slugify = function () {
+  this.slug = slug(this.title)
+}
 
-ArticleSchema.methods.updateFavoriteCount = function() {
-  var article = this;
+ArticleSchema.methods.updateFavoriteCount = function () {
+  const article = this
 
-  return User.count({favorites: {$in: [article._id]}}).then(function(count){
-    article.favoritesCount = count;
+  return User.count({favorites: {$in: [article._id]}}).then(count => {
+    article.favoritesCount = count
 
-    return article.save();
-  });
-};
+    return article.save()
+  })
+}
 
-ArticleSchema.methods.toJSONFor = function(user){
+ArticleSchema.methods.toJSONFor = function (user) {
   return {
     slug: this.slug,
     title: this.title,
@@ -48,7 +48,7 @@ ArticleSchema.methods.toJSONFor = function(user){
     favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
     author: this.author.toProfileJSONFor(user)
-  };
-};
+  }
+}
 
-mongoose.model('Article', ArticleSchema);
+mongoose.model('Article', ArticleSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,26 @@
-var jwt = require('express-jwt');
-var secret = require('../config').secret;
+const jwt = require('express-jwt')
+const secret = require('../config').secret
 
-function getTokenFromHeader(req){
+function getTokenFromHeader(req) {
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
-    return req.headers.authorization.split(' ')[1];
+    return req.headers.authorization.split(' ')[1]
   }
 
-  return null;
+  return null
 }
 
-var auth = {
+const auth = {
   required: jwt({
-    secret: secret,
+    secret,
     userProperty: 'payload',
     getToken: getTokenFromHeader
   }),
   optional: jwt({
-    secret: secret,
+    secret,
     userProperty: 'payload',
     credentialsRequired: false,
     getToken: getTokenFromHeader
   })
-};
+}
 
-module.exports = auth;
+module.exports = auth
